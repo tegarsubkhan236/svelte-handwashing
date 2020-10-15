559172d90cab390a50c7971ffeb62873e833b28e
tegarsubkhan236 <tegarsubkhan236@gmail.com>
2020-10-15 11:13:25 +0700

    html

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,14 @@
-<h1>Test</h1>
+<script>
+    import Timer from "./components/Timer.svelte";
+    import HowTo from "./components/HowTo.svelte";
+</script>
+
+<style>
+    h1 {
+        text-align: center;
+    }
+</style>
+
+<h1>Handwashing App</h1>
+<Timer />
+<HowTo />
